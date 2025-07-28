13de5ac44fe3dd79d437297e0904271660d977c1
Arianna Method <theariannamethod@gmail.com>
2025-07-29 02:50:44 +0300

    Simplify mini_le and server

--- arianna_core/__init__.py
+++ arianna_core/__init__.py
@@ -2,21 +2,6 @@ import importlib
 from .config import settings
 
 mini_le = importlib.import_module("arianna_core.mini_le")
-local_rag = importlib.import_module("arianna_core.local_rag")
-nanogpt_bridge = importlib.import_module("arianna_core.nanogpt_bridge")
-skin = importlib.import_module("arianna_core.skin")
-pain = importlib.import_module("arianna_core.pain")
-sixth_feeling = importlib.import_module("arianna_core.6th_feeling")
 server = importlib.import_module("arianna_core.server")
 
-__all__ = [
-    "mini_le",
-    "server",
-    "local_rag",
-    "nanogpt_bridge",
-    "skin",
-    "pain",
-    "sixth_feeling",
-    "settings",
-]
-
+__all__ = ["mini_le", "server", "settings"]
