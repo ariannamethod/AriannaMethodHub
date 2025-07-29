92dcac85c3272d2f27fc15419c23b14ddc928619
Arianna Method <theariannamethod@gmail.com>
2025-07-29 11:51:01 +0300

    Refine evolution_steps entries

--- arianna_core/__init__.py
+++ arianna_core/__init__.py
@@ -4,5 +4,6 @@ from .config import settings
 
 mini_le: Any = importlib.import_module("arianna_core.mini_le")
 server = importlib.import_module("arianna_core.server")
+evolution_steps = importlib.import_module("arianna_core.evolution_steps")
 
-__all__ = ["mini_le", "server", "settings"]
+__all__ = ["mini_le", "server", "evolution_steps", "settings"]
