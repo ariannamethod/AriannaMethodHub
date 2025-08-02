24edee5db9fa4a8d59b74e740bf63e48d65a818a
Arianna Method <theariannamethod@gmail.com>
2025-08-02 22:16:10 +0300

    Add bio state trackers and orchestrator

--- arianna_core/__init__.py
+++ arianna_core/__init__.py
@@ -5,5 +5,6 @@ from .config import settings
 mini_le: Any = importlib.import_module("arianna_core.mini_le")
 server = importlib.import_module("arianna_core.server")
 evolution_steps = importlib.import_module("arianna_core.evolution_steps")
+bio = importlib.import_module("arianna_core.bio")
 
-__all__ = ["mini_le", "server", "evolution_steps", "settings"]
+__all__ = ["mini_le", "server", "evolution_steps", "settings", "bio"]
